689fb60d4781c573dfb9f78c4d93cb5ede2eea90
Hadeer <Hadeerashraf189@gmail.com>
2021-06-07 16:59:51 +0200

    edit backgroundimage

--- src/WantToRead.js
+++ src/WantToRead.js
@@ -12,7 +12,7 @@ const WantToRead = (props)=> {
               <li key={book.id}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193,backgroundImage:`url(${book.imageLinks.thumbnail&&book.imageLinks.smallThumbnail})`}}></div>
+                  <div className="book-cover" style={{ width: 128, height: 193,backgroundImage:`url(${book.imageLinks ? book.imageLinks.thumbnail: ''})`}}></div>
                 </div>
                 <div className="book-title">{book.title}</div>
                 <div className="book-authors">{book.authors}</div>
