740ca4fc06451356f34795439827414daa0a6db3
Hadeer <Hadeerashraf189@gmail.com>
2021-06-09 03:23:52 +0200

    add select element

--- src/CurrentlyReading.js
+++ src/CurrentlyReading.js
@@ -1,7 +1,7 @@
 import  React from 'react';
 
 const CurrentlyReading = (props) => {
-    const {cBooks}=props
+    const {cBooks,onUpdate,onEvent}=props
         return ( 
           <React.Fragment>
                <div className="bookshelf">
@@ -13,6 +13,15 @@ const CurrentlyReading = (props) => {
                     <div className="book">
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193,backgroundImage:`url(${book.imageLinks ? book.imageLinks.thumbnail: ''})`}}></div>
+                        <div className="book-shelf-changer">
+                          <select defaultValue={book.shelf} onChangeCapture={()=>onUpdate(book,book.shelf)} onChange={(e)=>onEvent(e)}>
+                            <option  value="move" disabled >Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
                       </div>
                       <div className="book-title">{book.title}</div>
                       <div className="book-authors">{book.authors}</div>
